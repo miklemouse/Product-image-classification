3a07c33b3080d644a8f843869654f07c66072de1
miklemouse <32647568+miklemouse@users.noreply.github.com>
2021-08-16 14:48:30 +0300

    Add files via upload

--- label.py
+++ label.py
@@ -77,8 +77,8 @@ def check_main_image(item_number, image_folder_path):
     """
     main_img_name = str(item_number) + '-M.jpg'
     if path.exists(image_folder_path + main_img_name):
-        return (True, list_to_csv_str([main_img_name]))
-    return (False, '')
+        return (True, list_to_csv_str([main_img_name]), main_img_name)
+    return (False, '', '')
 
 
 def check_images(csv_urls, item_number, image_folder_path):
@@ -103,14 +103,15 @@ def check_images(csv_urls, item_number, image_folder_path):
         img_name = str(item_number) + '-' + str(img_number + 1) \
                    + '.jpg'
         if not path.exists(image_folder_path + img_name):
-            return (False, [])
+            return (False, '', [])
         img_names.append(img_name)
 
-    return (True, list_to_csv_str(img_names))
+    return (True, list_to_csv_str(img_names), img_names)
 
 
 def label(input_filename, output_filename, image_folder_path,
-          IMG_URLS_COL, MAIN_IMAGE_URL_COL, progress_check):
+          cat_by_img_csv, img_amount_by_cat_csv,
+          CAT_COL, IMG_URLS_COL, MAIN_IMAGE_URL_COL, progress_check):
     """Write information about images to a CSV file.
 
             Parameters:
@@ -123,6 +124,9 @@ def label(input_filename, output_filename, image_folder_path,
                     image_folder_path (str): Name of the folder
                     containing the images
     """
+    cat_by_img = dict()
+    img_amount_by_cat = dict()
+
     with open(input_filename, 'r') as input_file,\
          open(output_filename, 'w') as output_file:
         reader = csv.reader(input_file, delimiter=',', quotechar='"',
@@ -144,43 +148,67 @@ def label(input_filename, output_filename, image_folder_path,
                 writer.writerow(newline_row)
 
             else:
+                cat = int(row[CAT_COL])
+
                 if len(row) < 4 or row[MAIN_IMAGE_URL_COL] == '':
                     continue
-                m_img_exists, m_img_name = check_main_image(
-                                                line_number,
-                                                image_folder_path
-                                           )
+
+                m_img_exists, m_img_name, m_img_real_name = check_main_image(
+                                                                line_number,
+                                                                image_folder_path
+                                                            )
                 if m_img_exists:
                     newline_row.append(m_img_name)
                     csv_urls = row[IMG_URLS_COL]
-                    imgs_exist, img_names = check_images(
-                                                csv_urls,
-                                                line_number,
-                                                image_folder_path
-                                            )
+                    imgs_exist, img_names, real_img_names = check_images(
+                                                             csv_urls,
+                                                             line_number,
+                                                             image_folder_path
+                                                            )
+                    cat_by_img[m_img_real_name] = cat
+                    if not cat in img_amount_by_cat:
+                        img_amount_by_cat[cat] = [0, 0]
+                    img_amount_by_cat[cat][0] += 1
                     if imgs_exist:
                         newline_row.append(img_names)
                         writer.writerow(newline_row)
+                        for img_name in real_img_names:
+                            cat_by_img[img_name] = cat
+                            img_amount_by_cat[cat][1] += 1
                     else:
                         print("incomplete data", line_number,
                               img_names)
 
+    with open(cat_by_img_csv, 'w') as cat_by_img_file:
+        writer = csv.writer(cat_by_img_file)
+        for img, cat in cat_by_img.items():
+            writer.writerow([img, cat])
+
+    with open(img_amount_by_cat_csv, 'w') as img_amount_by_cat_file:
+        writer = csv.writer(img_amount_by_cat_file)
+        for cat, amount in img_amount_by_cat.items():
+            writer.writerow([cat] + amount)
 
 def main(argv):
 
+    CAT_COL = 1
     IMG_URLS_COL = 2
     MAIN_IMAGE_URL_COL = 3
 
     input_filename = ""
     image_folder_path = ""
     output_filename = ""
+    img_amount_by_cat_csv = ""
+    cat_by_img_csv = ""
     progress_check = 0
 
     try:
-        opts, _ = getopt.getopt(argv, "hf:i:o:p:",
+        opts, _ = getopt.getopt(argv, "hf:i:o:c:a:p:",
                                 ["input_file=",
                                  "img_folder=",
                                  "output_file=",
+                                 "cat_by_img_csv=",
+                                 "img_amount_by_cat_csv=",
                                  "progress_check="])
 
     except getopt.GetoptError:
@@ -190,10 +218,15 @@ def main(argv):
     for opt, arg in opts:
         if opt == '-h':
             print ('label.py -f <input_file> -i <img_folder> ' +
-                   '-o <output_file> -p [progress_check]')
+                   '-o <output_file> -c <cat_by_img_csv> ' +
+                   '-a <img_amount_by_cat_csv> -p [progress_check]')
             print('The CSV file with object info <input_file>')
             print('The folder with the downloaded images <img_folder>')
-            print('The file to write the info to <output_file>')
+            print('The file to write the general info <output_file>')
+            print('The file to write category of each image ' +
+                  '<cat_by_img_csv>')
+            print('The file to write image amount in each category ' +
+                  '<img_amount_by_cat_csv>')
             print('Print progress every [progress_check] lines')
             sys.exit()
         if opt in ["-f", "--input_file"]:
@@ -202,11 +235,16 @@ def main(argv):
             image_folder_path = arg
         elif opt in ["-o", "--output_file"]:
             output_filename = arg
+        elif opt in ["-c", "--cat_by_img_csv"]:
+            cat_by_img_csv = arg
+        elif opt in ["-a", "--img_amount_by_cat_csv"]:
+            img_amount_by_cat_csv = arg
         elif opt in ["-p", "--progress_check"]:
             progress_check = int(arg)
 
     if not input_filename or not image_folder_path or\
-       not output_filename:
+       not output_filename or not img_amount_by_cat_csv or\
+       not cat_by_img_csv:
         print ('label.py -h for help')
         sys.exit(2)
 
@@ -216,7 +254,8 @@ def main(argv):
     # Write information about images in the CSV file
     print("Labeling images...")
     label(input_filename, output_filename, image_folder_path,
-          IMG_URLS_COL, MAIN_IMAGE_URL_COL, progress_check)
+          cat_by_img_csv, img_amount_by_cat_csv,
+          CAT_COL, IMG_URLS_COL, MAIN_IMAGE_URL_COL, progress_check)
 
 
 if __name__ == '__main__':

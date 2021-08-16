0c1d559cbfbad51b921be64fa6e2c65305b7c2e2
miklemouse <32647568+miklemouse@users.noreply.github.com>
2021-08-16 18:26:46 +0300

    Add files via upload

--- download_and_preprocess.zsh
+++ download_and_preprocess.zsh
@@ -5,6 +5,9 @@ TYPE="$2"
 CATS="$3"
 IMGS="$4"
 
+CATS_="$5"
+IMGS_="$6"
+
 PREFIX=${FILE%.*}
 IMG_FOLDER="${PREFIX}_img_folder"
 FULL_INFO="${PREFIX}_full_info.csv"
@@ -20,4 +23,4 @@ python3 delete_duplicates.py -i $IMG_FOLDER
 
 python3 label.py -f $FILE -i $IMG_FOLDER -o $FULL_INFO -c $CAT_BY_IMG -a $IMG_AMOUNT
 
-python3 sort_by_class.py -i $IMG_FOLDER -o $SORTED -t $TYPE -n $IMGS -m $CATS -c $CAT_BY_IMG -a $IMG_AMOUNT
+python3 sort_by_class.py -i $IMG_FOLDER -o $SORTED -t $TYPE -n $IMGS_ -m $CATS_ -c $CAT_BY_IMG -a $IMG_AMOUNT
